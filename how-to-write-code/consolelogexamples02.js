934fe0d923034b034873f54409b18bfbc6a9d356
bosummz1884 <Brandonjulian16@gmail.com>
2024-03-26 09:11:57 -0500

    consolelog2

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,10 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+console.log("like\nusing\nthe\nokay");
+
+console.log("what\tdoes\tthe\ttab\tdo?");
+
+
+
